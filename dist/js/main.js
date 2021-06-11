0c3f03444fe65a08605ced06aabf03e3089d4bcf
Chef Kari <oskari.kivekas@student.lut.fi>
2021-06-11 16:09:22 +0300

    menu button animation

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuButton.addEventListener('click', toggleMenu);
+
+// hamburger menu functionality
+function toggleMenu() {
+    if(!showMenu){
+        menuButton.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    }
+    else{
+        menuButton.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        
+        showMenu = false;
+    }
+}
