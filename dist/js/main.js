c6f59b072fdbb3c56d7ebf18557bb50a87a8dbf2
Chef Kari <oskari.kivekas@student.lut.fi>
2021-06-14 13:34:51 +0300

    new menu design and animations: button click restriction needs to be added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,36 @@
-const menuButton = document.querySelector('.menu-button');
+const menuButton = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-
-
 const navItems = document.querySelectorAll('.nav-item');
-
+const container = document.querySelector('.container');
+const menuNav = document.querySelector('.menu-nav');
+//set initial menu state
 let showMenu = false;
 
 menuButton.addEventListener('click', toggleMenu);
 
-// hamburger menu functionality
-function toggleMenu() {
+
+
+function toggleMenu(){
     if(!showMenu){
+        container.classList.add('back');
         menuButton.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-
+        
+        // set menu state
         showMenu = true;
-    }
-    else{
+        setTimeout
+
+    }else{
+        container.classList.remove('back')
         menuButton.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
         
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set menu state
         showMenu = false;
     }
-}
+} 
